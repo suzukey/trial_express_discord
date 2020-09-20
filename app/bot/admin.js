e5df40a94b46207c47ba0a9242c1a7c4f6fc7582
suzukey <suzukei0208@gmail.com>
2020-09-20 19:13:37 +0900

    ✨ Add events

--- app/bot/admin.js
+++ app/bot/admin.js
@@ -2,8 +2,15 @@ const { Client } = require("discord.js")
 
 const adminClient = new Client()
 
+// クライアントが作業を開始する準備ができると発生します。
 adminClient.on("ready", async () => {
   console.log(`Bot logged in as ${adminClient.user.tag}`)
 })
+
+// ギルドメンバーが変更されるたびに呼び出されます。
+adminClient.on("guildMemberUpdate", (before, after) => {})
+
+// メンバーが音声状態を変更するたびに発生します。
+adminClient.on("voiceStateUpdate", (before, after) => {})
 
 module.exports = adminClient
--- app/bot/anonymously.js
+++ app/bot/anonymously.js
@@ -2,8 +2,18 @@ const { Client } = require("discord.js")
 
 const anonymouslyClient = new Client()
 
+// クライアントが作業を開始する準備ができると発生します。
 anonymouslyClient.on("ready", () => {
   console.log(`Bot logged in as ${anonymouslyClient.user.tag}`)
 })
+
+// ユーザーがチャンネルへの入力を開始するたびに発生します。
+anonymouslyClient.on("typingStart", (channel, user) => {})
+
+// メッセージが作成されるたびに発生します。
+anonymouslyClient.on("message", (message) => {})
+
+// メッセージが削除されるたびに発生します。
+anonymouslyClient.on("messageDelete", (message) => {})
 
 module.exports = anonymouslyClient
